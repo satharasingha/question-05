ae198ae91c5b1c149803c29c59613ca7d46ae027
Veru Satharasingha <Veru Satharasingha@DESKTOP-BN34K29>
2024-01-28 20:06:19 +0530

    Add project files.

---
+++ question 05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace question05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+
+            int number;
+            if (Int32.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Multiplication table of " + number + ":");
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(number + " x " + i + " = " + (number * i));
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+    }
+}
